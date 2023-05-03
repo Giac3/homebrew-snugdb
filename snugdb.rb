279d0b2f4b405792ba4bb215c22ceadca697d37e
Giacomo Pessotto <giacomopesso@gmail.com>
2023-05-03 14:27:45 +0100

    updated sha

--- snugdb.rb
+++ snugdb.rb
@@ -2,7 +2,7 @@ class Snugdb < Formula
   desc "SnugDB: A fun, simple, and local document database"
   homepage "https://github.com/Giac3/snugdb"
   url "https://github.com/Giac3/snugdb/archive/v0.0.1.tar.gz"
-  sha256 "bd16a5c861be536f7e4664efaf713cc1eab10a66a9014c6d87f6a3bfb68361f3"
+  sha256 "451464b6b2f226969347564756b52e401c9185481fc779b28d8ab0fde989f273"
   license "MIT"
 
   depends_on "cmake" => :build
